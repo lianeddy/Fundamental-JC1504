c37b6fd4fb433532b74c2b7ce1320cea9cd93ea7
Firliandy <lian.eddy@gmail.com>
2020-10-19 13:18:33 +0700

    commit

---
+++ satu.js
@@ -0,0 +1,27 @@
+// console.log("Hello"); // String dengan ' / "
+// console.log("World");
+// console.log("Hehe");
+
+// String
+// Number
+
+// slash dua kali untuk commment
+
+// Variabel / Variable
+var nama = "Lian";
+var job = "Coder";
+var exp = 2;
+// console.log(nama + " " + job); // String + String
+// console.log(10 + 5); // Number + Number
+// console.log(nama + 10); // String + Number = number diubah menjadi string -> string + string
+
+console.log(
+  "Halo nama saya " +
+    nama +
+    " pekerjaan saya adalah sebagai " +
+    job +
+    " sudah " +
+    exp +
+    " tahun"
+);
+console.log(exp);
