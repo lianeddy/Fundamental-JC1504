33c6fed1c07a7c10148f844630600c723d63c8c0
Firliandy <lian.eddy@gmail.com>
2020-11-06 11:15:55 +0700

    edit product

--- es6.js
+++ es6.js
@@ -18,6 +18,7 @@
 
 // Filter
 // Anonymous
+// var arr = [1, 2, 3];
 // var newArr = arr.filter(function (val) {
 //   return val % 2 === 0;
 // });
@@ -137,5 +138,118 @@
 //   name: newname,
 // });
 
-var nama = "bambang";
-console.log(nama.includes("mb"));
+// var nama = "bambang";
+// console.log(nama.includes("mb"));
+
+// find
+// findIndex
+
+var arrProduct = [
+  {
+    id: 15945893849,
+    category: "Fast Food",
+    name: "Noodle",
+    price: 5000,
+    stock: 9,
+  },
+  {
+    id: 15945893843,
+    category: "Electronic",
+    name: "Headphone",
+    price: 500000,
+    stock: 10,
+  },
+  {
+    id: 15945893844,
+    category: "Clothing",
+    name: "Hoodie",
+    price: 300000,
+    stock: 7,
+  },
+  { id: 15945893845, category: "Fruit", name: "Apple", price: 10000, stock: 8 },
+];
+
+// console.log(
+//   arrProduct.find((val) => {
+//     return val.id === 15945893843;
+//   })
+// );
+
+// console.log(
+//   arrProduct.findIndex((val) => {
+//     return val.id === 15945893843;
+//   })
+// );
+// Kondisi cart
+// If product sudah ada di dalam cart kita hanya tambah qty
+// else kita push product ke dalam arr cart
+
+// kalo kita buat kondisi dan didalam kondisi nya ada nama variabel
+// apa bila variable mempunyai nilai (ada isinya) kondisi terhitung true
+// jika isi variabel kosong atau (NaN, null, undefined), maka kondisi terhitung false
+// var nama = ["asd"];
+// if (!nama) {
+//   console.log("Hello");
+// }
+
+// Spread operatpr
+// var arr = [1, 2, 3];
+// var arr2 = [...arr, 4, 5, 6];
+//[1,2,3,4,5,6]
+// // console.log(arr2);
+
+// var obje = {
+//   nama: "Bambang",
+//   usia: 45,
+// };
+
+// var selectedProduct = {
+//   id: 15945893845,
+//   category: "Fruit",
+//   name: "Apple",
+//   price: 10000,
+//   stock: 8,
+// };
+
+// var cart = [];
+// cart.push({ nama: "appel", harga: 10000 });
+// cart.push({ ...obje, alamat: "bekasi", nikah: true });
+// console.log(cart);
+
+// var arr = [
+//   { nama: "lian", job: "coder" },
+//   { nama: "bambang", job: "karyawan" },
+//   { nama: "susilo", job: "pns" },
+// ];
+
+// var selectedFind = arr.find((val) => {
+//   return val.nama === "joko";
+// });
+
+// console.log(selectedFind);
+
+// if (selectedFind) {
+//   console.log("ada");
+// } else {
+//   console.log("tidak ada");
+// }
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var arrNew = arr.map((val) => {
+  if (val % 2 === 0) {
+    return val * 2;
+  } else {
+    return val / 2;
+  }
+});
+
+var newArr = [];
+for (let i = 0; i < array.length; i++) {
+  if (arr[i] % 2 === 0) {
+    newArr.push(arr[i] * 2);
+  } else {
+    newArr.push(arr[i] / 2);
+  }
+}
+
+console.log(arrNew);
